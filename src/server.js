3362aa6308dacccbe6c0abcb69bcdbdc0c48de42
Donuru Sindhuja Reddy <sindhujareddy1006@gmail.com>
2026-02-02 17:46:25 +0530

    removed seed logic from server.js

--- src/server.js
+++ src/server.js
@@ -1,9 +1,8 @@
-const seedSubjectsOnce = require("./seedOnBoot");
 require("dotenv").config();
 const app = require("./app");
 
 const PORT = process.env.PORT || 8080;
-seedSubjectsOnce().catch(console.error);
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
